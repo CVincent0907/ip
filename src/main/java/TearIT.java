3e793c97a4c9060def7346c39b92ffdce0c395e1
CVincent0907 <vincent045731@gmail.com>
2025-01-20 15:34:34 +0800

    Rename, Greet, Exit

--- src/main/java/TearIT.java
+++ src/main/java/TearIT.java
@@ -1,6 +1,24 @@
+import java.awt.*;
+
 public class TearIT {
+
+    private static final String opening = """
+            Hello! I'm TearIT.
+            What can I do for you?
+            
+            -------------------------------------------------""";
+
+
+    private static final String ending = """
+            Bye. Hope to see you again soon!
+           
+            -------------------------------------------------""";
+
+
     public static void main(String[] args) {
-        String
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-------------------------------------------------");
+        System.out.println(TearIT.opening);
+        System.out.println(TearIT.ending);
     }
+
 }
