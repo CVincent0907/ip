bfc6ba122d5a9ae408f500697d3ab3a8920ad0f8
CVincent0907 <vincent045731@gmail.com>
2025-01-20 17:35:32 +0800

    Added Task class to handle user input as Task object independently

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private static int taskCount = 0;
+    private final String description;
+
+    public static int getTaskCount() {
+        return Task.taskCount;
+    }
+
+    public static void addTaskCount() {
+        Task.taskCount++;
+    }
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
 }
