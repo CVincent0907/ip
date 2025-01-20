b95c0c3a8d2f2aed4965e7621aa988cb0755a993
CVincent0907 <vincent045731@gmail.com>
2025-01-20 17:11:18 +0800

    Added list functionality and store functionality for every user input except bye and list in echo function

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,20 +1,40 @@
 import java.util.Objects;
 import java.util.Scanner;
 public class Echo {
+    private static String[] taskArray = new String[100];
+    private static int taskCount = 0;
 
     //This function receives TearIt.ending as String input
     //This function prints out echo and ending message when user enter "bye"
     public static void echo(String ending) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        while (!Objects.equals(input, "bye")) {
-            System.out.println("    -------------------------------------------------");
-            System.out.println("    " + input);
-            System.out.println("    -------------------------------------------------");
-            input = sc.nextLine();
+        while (!Objects.equals(input.toLowerCase(), "bye")) {
+            if (Objects.equals(input.toLowerCase(), "list")) {
+                Echo.list();
+                input = sc.nextLine();
+            } else {
+                System.out.println("    -------------------------------------------------");
+                Echo.taskArray[Echo.taskCount] = input;
+                Echo.taskCount++;
+                System.out.println("      added: " + input);
+                System.out.println("    -------------------------------------------------");
+                input = sc.nextLine();
+            }
+
         }
         System.out.println("    -------------------------------------------------");
         System.out.println(ending);
+    }
+
+
+    // This is a list function that display all task in taskArray upon input list/LIST or its variant
+    public static void list() {
+        System.out.println("    -------------------------------------------------");
+        for (int i = 0; i < Echo.taskCount; i++) {
+            System.out.println("    " + (i+1) + ". " + Echo.taskArray[i]);
+        }
+        System.out.println("    -------------------------------------------------");
     }
 
 }
