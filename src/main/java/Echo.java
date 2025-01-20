325be92810700118566ac0e1cbda25d894b8315d
CVincent0907 <vincent045731@gmail.com>
2025-01-20 18:27:49 +0800

    Added mark, unmark and overloaded list function for task checking purpose

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -7,17 +7,26 @@ public class Echo {
     //This function prints out echo and ending message when user enter "bye"
     public static void echo(String ending) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        while (!Objects.equals(input.toLowerCase(), "bye")) {
-            if (Objects.equals(input.toLowerCase(), "list")) {
+
+        // might need to add in error handling for mark but without number
+        while (true) {
+            String input = sc.nextLine();
+            String[] parts = input.split(" ");
+            String part = parts[0];
+
+            if (Objects.equals(part.toLowerCase(), "list")) {
                 Echo.list();
-                input = sc.nextLine();
+            } else if (Objects.equals(part.toLowerCase(), "mark") && parts.length == 2) {
+                Echo.markRemark(Integer.parseInt(parts[1]));
+            } else if (Objects.equals(part.toLowerCase(), "unmark") && parts.length == 2) {
+                Echo.unmarkRemark(Integer.parseInt(parts[1]));
+            } else if (Objects.equals(part.toLowerCase(), "bye")) {
+                break;
             } else {
                 Echo.add(input);
-                input = sc.nextLine();
             }
-
         }
+
         System.out.println("-------------------------------------------------");
         System.out.println(ending);
     }
@@ -26,8 +35,26 @@ public class Echo {
     // This is a list function that display all task in taskArray upon input list/LIST or its variant
     public static void list() {
         System.out.println("    -------------------------------------------------");
+        System.out.println("      Here are the tasks in your list:");
         for (int i = 0; i < Task.getTaskCount(); i++) {
             System.out.println("    " + (i+1) + ". " + Echo.taskArray[i]);
+        }
+        System.out.println("    -------------------------------------------------");
+    }
+
+    // This is an overloaded list function that use to mark and display task by receiving input
+    // input: task number and boolean mark and String msg
+    public static void list(int i, boolean mark, String msg) {
+        if (mark) {
+            Echo.taskArray[i-1].mark();
+        } else {
+            Echo.taskArray[i-1].unmark();
+        }
+
+        System.out.println("    -------------------------------------------------");
+        System.out.println(msg);
+        for (int j = 0; j < Task.getTaskCount(); j++) {
+            System.out.println("    " + (j+1) + ". " + Echo.taskArray[j]);
         }
         System.out.println("    -------------------------------------------------");
     }
@@ -42,9 +69,17 @@ public class Echo {
         System.out.println("    -------------------------------------------------");
     }
 
+    // This function marks the ith numbered task done and display it
+    // Receive int i as input
+    public static void markRemark(int i) {
+        Echo.list(i, true, "    Well Done! You finished task " + i + ".");
+    }
 
-//    public static void mark(int num) {
-//
-//    }
+    // This function unmarks the ith numbered task done and display it
+    // Receive int i as input
+    public static void unmarkRemark(int i) {
+        Echo.list(i, false, "   Oh No! You haven't completed task " + i + ".");
+    }
+
 
 }
