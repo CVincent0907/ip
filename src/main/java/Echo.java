9a950058cf94967e75d97547be993e7f3d5c1c4a
CVincent0907 <vincent045731@gmail.com>
2025-01-20 16:14:58 +0800

    Added an echo looping function which gets user command and termintate when bye is received

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Echo {
+import java.util.Objects;
+import java.util.Scanner;
+public class Echo {
+
+    //This function receives TearIt.ending as String input
+    //This function prints out echo and ending message when user enter "bye"
+    public static void echo(String ending) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println("-------------------------------------------------");
+            System.out.println(input);
+            System.out.println("-------------------------------------------------");
+            input = sc.nextLine();
+        }
+        System.out.println("-------------------------------------------------");
+        System.out.println(ending);
+    }
+    
 }
