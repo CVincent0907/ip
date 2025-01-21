cc2d70c4618c56194bfda3a60d89f47974f5d36a
CVincent0907 <vincent045731@gmail.com>
2025-01-21 17:48:47 +0800

    Override to String method and add in regex to extract deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Deadline {
+public class Deadline extends Task {
+    private final String deadline;
+    public static final String regex = "(?i)^deadline\\s+([^/]+)\\s+/by\\s+(.+)$";
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
+    }
 }
