ac9932344d65e14d5606fbf756747648e7d0a53a
CVincent0907 <vincent045731@gmail.com>
2025-01-21 17:54:26 +0800

    Override toString method and add in regex for extraction of todo description

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Todo {
+public class Todo extends Task {
+    public static final String regex = "(?i)^todo\\s+(.+)$";
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+       return String.format("[T]%s ",super.toString());
+    }
 }
