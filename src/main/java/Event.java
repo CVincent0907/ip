88217945edea2809ac1dc8cd5b3cc086d1882685
CVincent0907 <vincent045731@gmail.com>
2025-01-21 17:50:19 +0800

    Override toString method and add in regex to extract from and to time alongside description

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Event {
+public class Event extends Task{
+
+    private final String fromTime;
+    private final String toTime;
+    public static final  String regex = "(?i)^event\\s+([^/]+)\\s+/from\\s+([^/]+)\\s+/to\\s+(.+)$";
+
+    public Event(String description, String fromTime, String toTime) {
+        super(description);
+        this.fromTime = fromTime;
+        this.toTime = toTime;
+
+    }
+
+    @Override
+    public String toString() {
+        String format = String.format("[E]%s (from: %s to: %s)", super.toString(), this.fromTime, this.toTime);
+        return format;
+    }
 }
