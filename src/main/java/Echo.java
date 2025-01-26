08df9b14ffaaa0717830426c52e40bb7891340e2
CVincent0907 <vincent045731@gmail.com>
2025-01-26 14:58:12 +0800

    Add extractTask, readFromFile and writeToFile methods to allow retrieval and storing of tasks into text format upon processing

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,15 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Echo {
-    private static final ArrayList<Task> taskArray = new ArrayList<>();
+    private static final ArrayList<Task> TASK_ARRAY = new ArrayList<>();
+    private static final String PATH = "src/main/data/TearIT.txt";
 
     //This function receives TearIt.ending as String input
     //This function prints out echo and ending message when user enter "bye"
     //input1: the ending messaged passed from tearIT
     public static void echo(String ending) {
+
+        try {
+            Echo.readFromFile(Echo.PATH);
+        } catch (FileNotFoundException e) {
+            System.out.println("The file does not exist !");
+        } finally {
+
+        }
+
+
         Scanner sc = new Scanner(System.in);
 
         // ErrorCode added in to handle the command which is NOT TODO, DEADLINE, EVENT
@@ -68,6 +84,16 @@ public class Echo {
 
                     break;
                 case "bye":
+
+                    try {
+                            Echo.writeToFile(Echo.PATH);
+                    } catch (IOException e) {
+                        System.out.println("    Something went wrong: " + e.getMessage());
+                        System.out.println("    The data is not saved !");
+                    } finally {
+
+                    }
+
                     break label;
                 case "delete":
                     if (len == 1) {
@@ -88,9 +114,9 @@ public class Echo {
                 default:
                     int errorCode = Echo.add(input);
                     final int ERROR_CODE_UNKNOWN_COMMAND = 0;
-                    final int ERROR_CODE_TODO_COMMAND = 1;
-                    final int ERROR_CODE_DEADLINE_COMMAND = 2;
-                    final int ERROR_CODE_EVENT_COMMAND = 3;
+                    final int ERROR_CODE_TODO_COMMAND = -1;
+                    final int ERROR_CODE_DEADLINE_COMMAND = -2;
+                    final int ERROR_CODE_EVENT_COMMAND = -3;
 
                     // should also specify input format to user also like from ... /to .... TODO
                     switch (errorCode) {
@@ -112,7 +138,7 @@ public class Echo {
                 }
             }
         }
-        
+
 
         System.out.println("-------------------------------------------------");
         System.out.println(ending);
@@ -120,47 +146,47 @@ public class Echo {
     }
 
 
-    // This is a list function that display all task in taskArray upon input list/LIST or its variant
+    // This is a list function that display all task in TASK_ARRAY upon input list/LIST or its variant
     // input:None
-    public static void list() {
+    private static void list() {
         System.out.println("    -------------------------------------------------");
         System.out.println("    Here are the tasks in your list:");
         for (int i = 0; i < Task.getTaskCount(); i++) {
-            System.out.println("    " + (i + 1) + ". " + Echo.taskArray.get(i));
+            System.out.println("    " + (i + 1) + ". " + Echo.TASK_ARRAY.get(i));
         }
         System.out.println("    -------------------------------------------------");
     }
 
     // This is an overloaded list function that use to mark and display task by receiving input
     // input1: task order input2: mark or unmark(true or false) input3: String msg (congratz or humiliating msg based on completion)
-    public static void list(Integer i, boolean mark, String msg) {
+    private static void list(Integer i, boolean mark, String msg) {
         if (i <= Task.getTaskCount() && i != 0) {
             if (mark) {
-                Echo.taskArray.get(i - 1).mark();
+                Echo.TASK_ARRAY.get(i - 1).mark();
             } else {
-                Echo.taskArray.get(i - 1).unmark();
+                Echo.TASK_ARRAY.get(i - 1).unmark();
             }
             System.out.println("    -------------------------------------------------");
             System.out.println(msg + i + " .");
             for (int j = 0; j < Task.getTaskCount(); j++) {
-                System.out.println("    " + (j + 1) + ". " + Echo.taskArray.get(j));
+                System.out.println("    " + (j + 1) + ". " + Echo.TASK_ARRAY.get(j));
             }
             System.out.println("    -------------------------------------------------");
         } else {
             System.out.println("    -------------------------------------------------");
             System.out.println("You do not have task " + i + "!");
             for (int j = 0; j < Task.getTaskCount(); j++) {
-                System.out.println("    " + (j + 1) + ". " + Echo.taskArray.get(j));
+                System.out.println("    " + (j + 1) + ". " + Echo.TASK_ARRAY.get(j));
             }
             System.out.println("    -------------------------------------------------");
         }
 
     }
 
-    // This function add every user input  except mark/list/bye as task to taskArray
+    // This function add every user input  except mark/list/bye as task to TASK_ARRAY
     // If none of them match.... REMEMBER to add in handle (we only handle for TODO, DEADLINE and EVENT)
     // Receive input 1: task description input 2: task type (event, deadline, todo)
-    public static int add(String input) {
+    private static int add(String input) {
         System.out.println("    -------------------------------------------------");
         String[] parts = input.split(" ");
         int len = parts.length;
@@ -185,7 +211,7 @@ public class Echo {
         }
 
         System.out.println("    Got it. I've added this task:");
-        String message1 = String.format("       %s", Echo.taskArray.get(Task.getTaskCount()-1));
+        String message1 = String.format("       %s", Echo.TASK_ARRAY.get(Task.getTaskCount()-1));
         System.out.println(message1);
 
         String message2 = String.format("    Now you have %d tasks in the list.", Task.getTaskCount());
@@ -198,12 +224,12 @@ public class Echo {
     // This is a delete function where user can use it to delete task
     // input1: An integer
 
-    public static void delete(Integer i)
+    private static void delete(Integer i)
             throws NumberFormatException {
         System.out.println("    -------------------------------------------------");
         if (i <= Task.getTaskCount() && i != 0) {
-            String message = String.format("    %s", Echo.taskArray.get(i - 1).toString());
-            Echo.taskArray.remove(i - 1);
+            String message = String.format("    %s", Echo.TASK_ARRAY.get(i - 1).toString());
+            Echo.TASK_ARRAY.remove(i - 1);
             Task.reduceTaskCount();
             System.out.println("    Hey bro! I have removed task " + i + ".");
             System.out.println(message);
@@ -217,32 +243,97 @@ public class Echo {
 
     // This function marks the ith numbered task done and display it
     // input1 : the order of task
-    public static void markRemark(Integer i) {
+    private static void markRemark(Integer i) {
         Echo.list(i, true, "    Well Done! You finished task ");
     }
 
     // This function unmarks the ith numbered task done and display it
     // input1 : the order of task
-    public static void unmarkRemark(Integer i) {
+    private static void unmarkRemark(Integer i) {
         Echo.list(i, false, "    Oh No! You haven't completed task ");
     }
 
     // This function extract the info from user input via regex and create the corresponding task object
     // This function also adds taskCount for all objects created here [ie: only Deadline, Event, Todo]
     // input1:regex input2:userinput input3: number of needed group
-    public static void extractAndCreate(String userInput, String regex, int groups ) {
+    private static void extractAndCreate(String userInput, String regex, int groups ) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(userInput);
+
         if (matcher.find()) {
             if (groups == 1) {
-                Echo.taskArray.add(new Todo(matcher.group(1)));
+                Echo.TASK_ARRAY.add(new Todo(matcher.group(1)));
             } else if (groups == 2) {
-                Echo.taskArray.add(new Deadline(matcher.group(1), matcher.group(2)));
+                Echo.TASK_ARRAY.add(new Deadline(matcher.group(1), matcher.group(2)));
             } else {
-                Echo.taskArray.add(new Event(matcher.group(1), matcher.group(2), matcher.group(3)));
+                Echo.TASK_ARRAY.add(new Event(matcher.group(1), matcher.group(2), matcher.group(3)));
             }
         }
-        Task.addTaskCount();
+        Task.addTaskCount(); // TODO this can be done when new task created in respective task class
+    }
+
+
+    // This function is used to extract test file string line by line into a task object
+    // input1: task string from text file
+    public static void extractTask(String task) {
+
+        if (!Objects.equals(task, "")) {
+            // Regex for each task type
+            Pattern todoPattern = Pattern.compile("\\[T\\]\\[(X| )\\] (.*)");
+            Pattern deadlinePattern = Pattern.compile("\\[D\\]\\[(X| )\\] (.*) \\(by: (.*)\\)");
+            Pattern eventPattern = Pattern.compile("\\[E\\]\\[(X| )\\] (.*) \\(from: (.*) to: (.*)\\)");
+
+            Matcher todoMatcher = todoPattern.matcher(task);
+            Matcher deadlineMatcher = deadlinePattern.matcher(task);
+            Matcher eventMatcher = eventPattern.matcher(task);
+
+            // Match Todo task
+            if (todoMatcher.matches()) {
+                boolean isDone = todoMatcher.group(1).equals("X");
+                TASK_ARRAY.add(new Todo(todoMatcher.group(2)));
+                if (isDone) {
+                    TASK_ARRAY.get(Task.getTaskCount()).mark();
+                }
+
+            }
+
+            // Match Deadline task
+            if (deadlineMatcher.matches()) {
+                boolean isDone = deadlineMatcher.group(1).equals("X");
+                TASK_ARRAY.add(new Deadline(deadlineMatcher.group(2), deadlineMatcher.group(3)));
+                if (isDone) {
+                    TASK_ARRAY.get(Task.getTaskCount()).mark();
+                }
+            }
+
+            // Match Event task
+            if (eventMatcher.matches()) {
+                boolean isDone = eventMatcher.group(1).equals("X");
+                TASK_ARRAY.add(new Event(eventMatcher.group(2), eventMatcher.group(3), eventMatcher.group(4)));
+                if (isDone) {
+                    TASK_ARRAY.get(Task.getTaskCount()).mark();
+                }
+            }
+
+            Task.addTaskCount();
+        }
+
+    }
+
+    private static void writeToFile(String filePath) throws IOException {
+        FileWriter fw = new FileWriter(filePath, false); //overwrite them
+        for (int i = 0; i < Task.getTaskCount(); i++) {
+            fw.write(TASK_ARRAY.get(i).toString() + System.lineSeparator());
+        }
+        fw.close();
+    }
+
+    private static void readFromFile(String filePath) throws FileNotFoundException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            Echo.extractTask(s.nextLine());
+        }
     }
 
 }
