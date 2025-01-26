7a1b0b9fb6379c66dacfdd30fe30a10247cf70c8
CVincent0907 <vincent045731@gmail.com>
2025-01-26 22:02:15 +0800

    Add Parser.java, Storage.java, Ui.java, Tasklist.java to implement more OOP

---
+++ src/main/java/Storage.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Storage {
+    private static final String PATH = "src/main/data/TearIT.txt";
+    public static void writeToFile(String filePath) throws IOException {
+        FileWriter fw = new FileWriter(filePath, false); //overwrite them
+        for (int i = 0; i < Task.getTaskCount(); i++) {
+            fw.write(Tasklist.getTaskString(i) + System.lineSeparator());
+        }
+        fw.close();
+    }
+
+    public static void readFromFile(String filePath) throws FileNotFoundException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            Parser.extractTaskFromFile(s.nextLine());
+        }
+    }
+
+    public static String getPath() {
+        return Storage.PATH;
+    }
+
+}
