18f46de0a8db3cdf67e9acf01cd395ed6e7767f8
CVincent0907 <vincent045731@gmail.com>
2025-01-26 22:03:34 +0800

    Update the regex to handle invalid dateTime in Deadline and Event.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,8 +3,12 @@ import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     private final LocalDateTime deadline;
-    public static final String DATE_TIME_REGEX_1 = "deadline (.*?) /by (\\d{1,2}-\\d{1,2}-\\d{4} \\d{4})"; // regex to recognize full date time 12-08-2027 1800 2027 and 1800 must be 4 digits
-    public static final String DATE_TIME_REGEX_2= "\\[D\\]\\[(X| )\\] (.*?) \\(by: (\\w+ \\d{1,2} \\d{4} \\d{1,2} [APMapm]{2})\\)";
+    public static final String DATE_TIME_REGEX_1 =
+            "deadline\\s+(.*?)\\s+/by\\s+(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})\\s+(00|01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23)([0-5][0-9])";
+
+    // regex to recognize full date time 12-08-2027 1800 2027 and 1800 must be 4 digits
+    public static final String DATE_TIME_REGEX_2 = "\\[D\\]\\[(X| )\\] (.*?) \\(by: (\\w+ \\d{1,2} \\d{4} \\d{1,2}:\\d{2} [APMapm]{2})\\)";
+
     public Deadline(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
@@ -12,6 +16,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy hh a")));
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a")));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -5,10 +5,14 @@ public class Event extends Task {
 
     private final LocalDateTime fromTime;
     private final LocalDateTime toTime;
+
     public static final String DATE_TIME_REGEX_1 =
-            "event\\s+(.*?)\\s+/from\\s+(\\d{1,2}-\\d{1,2}-\\d{4}\\s+\\d{4})\\s+/to\\s+(\\d{1,2}-\\d{1,2}-\\d{4})";
-    public static final String DATE_TIME_REGEX_2 = "\\[E\\]\\[(X| )\\] (.*?) \\(from: (\\w+ \\d{1,2} \\d{4} \\d{1,2} " +
-            "[APMapm]{2}) to: (\\w+ \\d{1,2} \\d{4} \\d{1,2} [APMapm]{2})\\)";
+            "event\\s+(.*?)\\s+/from\\s+(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})\\s+" +
+                    "(00|01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23)([0-5][0-9])\\s" +
+                    "+/to\\s+(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})\\s+(00|01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23)([0-5][0-9])";
+
+    public static final String DATE_TIME_REGEX_2 = "\\[E\\]\\[(X| )\\] (.*?) \\(from: (\\w+ \\d{1,2} \\d{4} \\d{1,2}:\\d{2} [APMapm]{2}) to: (\\w+ \\d{1,2} \\d{4} \\d{1,2}:\\d{2} [APMapm]{2})\\)";
+
 
 
     public Event(String description, LocalDateTime fromTime, LocalDateTime toTime) {
@@ -21,6 +25,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.fromTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hh a")), this.toTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hh a")));
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.fromTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a")), this.toTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a")));
     }
 }
