fd4f66e1b3a7dcc33e65f7c257fe8fbc38c8b8e1
CVincent0907 <vincent045731@gmail.com>
2025-01-27 16:46:53 +0800

    Add TasklistTest.java file to accomodate junit tests for add and delete methods in Tasklist.java

--- src/test/java/test/TasklistTest.java
+++ src/test/java/test/TasklistTest.java
@@ -1,2 +1,96 @@
-package test;public class TasklistTest {
+package test;
+
+import org.junit.jupiter.api.Test;
+import task.Tasklist;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TasklistTest {
+
+
+    // Test todo without any arg
+    @Test
+    public void add_toDoNoArg_fail() {
+        assertEquals(-1, Tasklist.add("todo "));
+    }
+
+    // Test todo with valid arg
+    @Test
+    public void add_toDoValidArg_success() {
+        assertEquals(1, Tasklist.add("todo read book"));
+    }
+
+    // Test todo with invalid arg
+    @Test
+    public void add_toDoInValidArg_success() {
+        assertEquals(1, Tasklist.add("todo read book"));
+    }
+
+
+    // Test deadline without any arg
+    @Test
+    public void add_deadlineNoArg_fail() {
+        assertEquals(-2, Tasklist.add("deadline "));
+    }
+
+    // Test deadline with  valid arg
+    @Test
+    public void add_deadlineValidArg_success() {
+        assertEquals(1, Tasklist.add("deadline read book /by 12-08-2024 1800"));
+    }
+
+    // Test deadline with arg
+    @Test
+    public void add_deadlineInvalidArg_success() {
+        assertEquals(1, Tasklist.add("deadline read book /by12-08-2024 1800"));
+    }
+
+    // Test event without any arg
+    @Test
+    public void add_eventNoArg_fail() {
+        assertEquals(-3, Tasklist.add("event "));
+    }
+
+
+    // Test event with valid arg
+    @Test
+    public void add_eventValidArg_success() {
+        assertEquals(1, Tasklist.add("event sport /from 12-08-2024 1800 /to 13-08-2024 1800"));
+    }
+
+    // Test event with invalid arg
+    @Test
+    public void add_eventInvalidArg_success() {
+        assertEquals(1, Tasklist.add("event sport /from12-08-2024 1800 /to 13-08-2024 1800"));
+    }
+
+    // Test add with invalid command
+    @Test
+    public void add_eventInvalidCommand_fail_1() {
+        assertEquals(0, Tasklist.add(" "));
+    }
+
+    // Test add with invalid command
+    @Test
+    public void add_eventInvalidCommand_fail_2() {
+        assertEquals(0, Tasklist.add("hahaha"));
+    }
+
+
+    // Test delete with invalid input type String
+    @Test
+    public void delete_invalidInputTypeString_fail_2() {
+        try {
+            String input = "hahaha";
+            Tasklist.delete(Integer.parseInt(input));
+            fail("Expected NumberFormatException was not thrown");
+        } catch (NumberFormatException e) {
+            assertEquals("For input string: \"hahaha\"", e.getMessage());
+        }
+    }
+
+
 }
