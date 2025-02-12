8531b2ec79bbf8d669555c80518066fa661cff10
CVincent0907 <vincent045731@gmail.com>
2025-02-12 19:28:27 +0800

    Add enable assertion will affect run comment

--- src/main/java/TearIT/TearIT.java
+++ src/main/java/TearIT/TearIT.java
@@ -10,6 +10,7 @@ public class TearIT {
 
     public String getResponse(String input) {
         // Assume Ui.echo() will got give null response despite any input by users
+        // Disabled now to remove side effects during run command
         assert Ui.echo(TearIT.ending, input) != "" : "Wrong assumption ! The return value is an empty strings";
         return Ui.echo(TearIT.ending, input);
     }
